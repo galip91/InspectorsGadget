554322940272c7f10c704a2cb19491f9bec3859f
Crater <craterboy@gmail.com>
2014-10-30 13:19:16 +0100

    GALIP IS SUPA SUPA SUPA SUPA SUPA ASUMMMEE

--- app/src/main/java/galip/hro/inspectorsgadget/MainActivity.java
+++ app/src/main/java/galip/hro/inspectorsgadget/MainActivity.java
@@ -38,6 +38,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.HashSet;
 import java.util.LinkedList;
 
 //import com.google.android.glass.widget.CardBuilder;
@@ -290,91 +291,95 @@ public class MainActivity extends Activity {
     private void onPhotoTaken(final String picturePath) {
         _taken = true;
 
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inSampleSize = 4;
+        OCR ocr = new OCR();
+        HashSet<String> result = ocr.findContainerNumbers(picturePath);
 
-        String _path = picturePath;
-        Bitmap bitmap = BitmapFactory.decodeFile(_path, options);
 
-        try {
-            ExifInterface exif = new ExifInterface(_path);
-            int exifOrientation = exif.getAttributeInt(
-                    ExifInterface.TAG_ORIENTATION,
-                    ExifInterface.ORIENTATION_NORMAL);
-
-            Log.v(TAG, "Orient: " + exifOrientation);
-
-            int rotate = 0;
-
-            switch (exifOrientation) {
-                case ExifInterface.ORIENTATION_ROTATE_90:
-                    rotate = 90;
-                    break;
-                case ExifInterface.ORIENTATION_ROTATE_180:
-                    rotate = 180;
-                    break;
-                case ExifInterface.ORIENTATION_ROTATE_270:
-                    rotate = 270;
-                    break;
-            }
-
-            Log.v(TAG, "Rotation: " + rotate);
-
-            if (rotate != 0) {
-
-                // Getting width & height of the given image
-                int w = bitmap.getWidth();
-                int h = bitmap.getHeight();
-
-                // Setting pre rotate
-                Matrix mtx = new Matrix();
-                mtx.preRotate(rotate);
-
-                // Rotating Bitmap
-                bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
-            }
-
-            // Convert to ARGB_8888, required by tess
-            bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
-
-        } catch (IOException e) {
-            Log.e(TAG, "Couldn't correct orientation: " + e.toString());
-        }
-
-        // _image.setImageBitmap( bitmap );
-
-        Log.v(TAG, "Before baseApi");
-
-        String DATA_PATH2 = Environment
-                .getExternalStorageDirectory().toString() + "/tessdata/";
-
-        TessBaseAPI baseApi = new TessBaseAPI();
-        baseApi.setDebug(true);
-        //baseApi.init(DATA_PATH2, lang);
-        //baseApi.setImage(bitmap);
-
-        //String recognizedText = baseApi.getUTF8Text();
-        String recognizedText = "Bla";
-        //baseApi.end();
-
-        // You now have the text in recognizedText var, you can do anything with it.
-        // We will display a stripped out trimmed alpha-numeric version of it (if lang is eng)
-        // so that garbage doesn't make it to the display.
-
-        Log.v(TAG, "OCRED TEXT: " + recognizedText);
-
-        if ( lang.equalsIgnoreCase("eng") ) {
-            recognizedText = recognizedText.replaceAll("[^a-zA-Z0-9]+", " ");
-        }
-
-        recognizedText = recognizedText.trim();
-
-        if ( recognizedText.length() != 0 ) {
-//            _field.setText(_field.getText().toString().length() == 0 ? recognizedText : _field.getText() + " " + recognizedText);
-//            _field.setSelection(_field.getText().toString().length());
-        }
-
-        // Cycle done.
+//        BitmapFactory.Options options = new BitmapFactory.Options();
+//        options.inSampleSize = 4;
+//
+//        String _path = picturePath;
+//        Bitmap bitmap = BitmapFactory.decodeFile(_path, options);
+//
+//        try {
+//            ExifInterface exif = new ExifInterface(_path);
+//            int exifOrientation = exif.getAttributeInt(
+//                    ExifInterface.TAG_ORIENTATION,
+//                    ExifInterface.ORIENTATION_NORMAL);
+//
+//            Log.v(TAG, "Orient: " + exifOrientation);
+//
+//            int rotate = 0;
+//
+//            switch (exifOrientation) {
+//                case ExifInterface.ORIENTATION_ROTATE_90:
+//                    rotate = 90;
+//                    break;
+//                case ExifInterface.ORIENTATION_ROTATE_180:
+//                    rotate = 180;
+//                    break;
+//                case ExifInterface.ORIENTATION_ROTATE_270:
+//                    rotate = 270;
+//                    break;
+//            }
+//
+//            Log.v(TAG, "Rotation: " + rotate);
+//
+//            if (rotate != 0) {
+//
+//                // Getting width & height of the given image
+//                int w = bitmap.getWidth();
+//                int h = bitmap.getHeight();
+//
+//                // Setting pre rotate
+//                Matrix mtx = new Matrix();
+//                mtx.preRotate(rotate);
+//
+//                // Rotating Bitmap
+//                bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
+//            }
+//
+//            // Convert to ARGB_8888, required by tess
+//            bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+//
+//        } catch (IOException e) {
+//            Log.e(TAG, "Couldn't correct orientation: " + e.toString());
+//        }
+//
+//        // _image.setImageBitmap( bitmap );
+//
+//        Log.v(TAG, "Before baseApi");
+//
+//        String DATA_PATH2 = Environment
+//                .getExternalStorageDirectory().toString() + "/tessdata/";
+//
+//        TessBaseAPI baseApi = new TessBaseAPI();
+//        baseApi.setDebug(true);
+//        //baseApi.init(DATA_PATH2, lang);
+//        //baseApi.setImage(bitmap);
+//
+//        //String recognizedText = baseApi.getUTF8Text();
+//        String recognizedText = "Bla";
+//        //baseApi.end();
+//
+//        // You now have the text in recognizedText var, you can do anything with it.
+//        // We will display a stripped out trimmed alpha-numeric version of it (if lang is eng)
+//        // so that garbage doesn't make it to the display.
+//
+//        Log.v(TAG, "OCRED TEXT: " + recognizedText);
+//
+//        if ( lang.equalsIgnoreCase("eng") ) {
+//            recognizedText = recognizedText.replaceAll("[^a-zA-Z0-9]+", " ");
+//        }
+//
+//        recognizedText = recognizedText.trim();
+//
+//        if ( recognizedText.length() != 0 ) {
+////            _field.setText(_field.getText().toString().length() == 0 ? recognizedText : _field.getText() + " " + recognizedText);
+////            _field.setSelection(_field.getText().toString().length());
+//        }
+//
+//        // Cycle done.
     }
 
 
